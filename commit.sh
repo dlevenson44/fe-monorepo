6944b5bcccaf400f9d2ae820fd12cb96ba9bcb7a
Dan Levenson <28206797+dlevenson44@users.noreply.github.com>
2023-11-16 20:50:22 -0500

    Changeset setup (#3)
    * feat: add changeset support, initialize commitizen
    
    * chore: first changeset
    
    * feat: add commit script!
    
    * fix: add prod buil support to commit script

---
+++ commit.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# App Root Directory
+current_directory=$(pwd)
+# Default changeset file check to false
+changeset_check=1
+
+
+# Check if changeset file exists
+changeset_existance_check () {
+  changeset_path=$(pwd)/.changeset
+  cd $changeset_path
+  for file in *; do
+    # Check for non-README markdown file, if found update changeset_check
+    if [ $(echo -n $file | tail -c 3 ) == ".md" ] && [ $file != "README.md" ] ; 
+    then
+      changeset_check=0
+    fi
+  done;
+}
+
+changeset_existance_check
+
+# Check if changeset exists
+if [ $changeset_check == 0 ] ;
+then
+  echo "changeset exists, continuing..."
+else
+  echo "changeset doesn't exist, creating..."
+  pnpm changeset
+fi
+
+# Check if user needs snapshot build
+read -p "Generate snapshot build (y/n): " generate_snapshot_build
+
+# Execute snapshot build based on user input
+shopt -s nocasematch
+if [[ $generate_snapshot_build == "y" ]] ;
+then
+  echo "generating snapshot version..."
+  pnpm changeset version --snapshot
+fi
+
+# Check if user needs Prod build
+read -p "Generate Prod build (y/n): " generate_prod_build
+if [[ $generate_prod_build == "y" ]] ;
+then
+  echo "generating prod version..."
+  pnpm changeset version
+fi
+
+# lint, test (when added), build, commit
+pnpm lint && pnpm build && pnpm cz
