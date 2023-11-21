a66210b2d90f459cd2bb7b8e81f29b38c81d8f15
Dan Levenson <28206797+dlevenson44@users.noreply.github.com>
2023-11-21 15:23:40 -0500

    feat: gh bot test setup (#4)
    * feat: gh bot test setup
    
    * fix: fix gh username actor
    
    * fix: test removing if statement
    
    * fix: gh action test fix
    
    * fix: another bot test fix
    
    * fix: remove token check
    
    * fix: fix gh token ref
    
    * fix: add gh action condition
    
    * fix: upgrade auto approve bot version
    
    * fix: revert on action
    
    * fix: update action token
    
    * fix: tweak token name
    
    * fix: fix token ref again hehe
    
    * fix: fix event type for PAT
    
    * fix: tweak perm
    
    * revert: revert on
    
    * revert on type
    
    * revert on type
    
    * tweak permissions
    
    * tweak name
    
    * add basic example
    
    * add custom message example
    
    * run action from another user
    
    * try adding env to step
    
    * ugh..
    
    * change on back to pull_req
    
    * try specifying branch
    
    * tweak on type
    
    * try deprecated version
    
    * tweak spelling thing
    
    * chatgpt attempt
    
    * tweak on setup
    
    * tweak auto approve step
    
    * bump dep
    
    * chatgpt take 2
    
    * update action usage
    
    * remove if conditiong
    
    * new chatgpt test
    
    * updated example to use target
    
    * new thing
    
    * ugh
    
    * add cleaned up workflow
    
    * fix run options

--- commit.sh
+++ commit.sh
@@ -20,20 +20,26 @@ changeset_existance_check () {
 
 changeset_existance_check
 
+shopt -s nocasematch
+
 # Check if changeset exists
 if [ $changeset_check == 0 ] ;
 then
   echo "changeset exists, continuing..."
 else
-  echo "changeset doesn't exist, creating..."
-  pnpm changeset
+  # echo "changeset doesn't exist, creating..."
+  read -p "no changeset found, would you like to generate one? (y/n): " generate_changeset
+  if [[ $generate_changeset == "y" ]] ;
+  then
+    echo "generating changeset..."
+    pnpm changeset
+  fi
 fi
 
 # Check if user needs snapshot build
 read -p "Generate snapshot build (y/n): " generate_snapshot_build
 
 # Execute snapshot build based on user input
-shopt -s nocasematch
 if [[ $generate_snapshot_build == "y" ]] ;
 then
   echo "generating snapshot version..."
