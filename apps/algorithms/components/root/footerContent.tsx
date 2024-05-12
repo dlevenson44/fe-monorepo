0f1a907210e956a77a045648e54fae51530a3d4f
dlevenson44 <daniellevenson4@gmail.com>
2024-05-11 18:44:01 -0700

    fix: tailwind fix on ui-components, footer content fix

--- apps/algorithms/components/root/footerContent.tsx
+++ apps/algorithms/components/root/footerContent.tsx
@@ -1,17 +1,16 @@
 import type { FC } from 'react'
 
 const FooterContent: FC = () => (
-  <div>
-    <p>
-      This website was built on notes taken from{' '}
-      <a
-        className={`font-medium text-sky-700`}
-        href="https://www.khanacademy.org/computing/computer-science/algorithms"
-      >
-        Khan Academy's Algorithms course
-      </a>
-    </p>
-  </div>
+  <>
+    This website was built on notes taken from{' '}
+    <a
+      className={`font-medium text-sky-700`}
+      href="https://www.khanacademy.org/computing/computer-science/algorithms"
+    >
+      Khan Academy's Algorithms course
+    </a>
+    .
+  </>
 )
 
 export default FooterContent
