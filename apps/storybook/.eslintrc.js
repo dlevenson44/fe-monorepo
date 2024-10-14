28d023fb7e5a09d6eb2245fa057cfd32711d914b
dlevenson44 <daniellevenson4@gmail.com>
2024-10-14 17:48:09 -0400

    feat: setup Storybook app

--- apps/storybook/.eslintrc.js
+++ apps/storybook/.eslintrc.js
@@ -2,9 +2,4 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `@config/eslint-config`
   extends: ['@config/eslint-config/index.js'],
-  settings: {
-      next: {
-          rootDir: ['apps/*/'],
-      },
-  },
 }
