1e2ff51517290201c316e0b89b942359ecf5c60b
dlevenson44 <daniellevenson4@gmail.com>
2025-11-03 22:29:50 -0500

    fix stories rendering

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -10,9 +10,9 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')))
 }
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)', // Make sure tsx is here
   ],
   "addons": [
     getAbsolutePath('@chromatic-com/storybook'),
--- apps/docs/stories/Header.tsx
+++ apps/docs/stories/Header.tsx
@@ -1,4 +1,4 @@
-import { Button } from './Button'
+import { Button } from 'dur-ui'
 import './header.css'
 
 type User = {
